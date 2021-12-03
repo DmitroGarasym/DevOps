146fb834e1a85c93abc680a3be41e9526b9e55bc
Дмитро Гарасим <94192861+DmitroGarasym@users.noreply.github.com>
2021-12-03 11:28:28 +0200

    Update script.sh

--- Lab3/script.sh
+++ Lab3/script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
+# Find and print names of all soft-links on your VM
+find / -type l -exec ls -la '{}'\;
 
-#find names of all soft-links on VM
-find / -type l -ls 
+# Find and print count of block and character devices
+find / -type b -or -type c | wc -l 
 
-#find count of block and character devices
-find / -type c -or -type b | wc -l 
+# Find all folders with Sticky bit
+find / -type d -perm -1000 -exec ls -ld {} \;
 
-#find all folders with Sticky bit
-find / -type d -perm /1000 -ls >> 
+# Make soft link for /etc/hostname in /tmp folder
+ln -s /etc/hostname /tmp/newhostname
 
-#make soft link for /etc/hostname in /tmp folder
-ln -s /etc/hostname /tmp/link
-
-#create user “testuser”
+# Create user “testuser”
 useradd testuser
 
-#create file in home directory “testuser_data” owned by “testuser”
-sudo -u testuser touch "/home/testuser/testuser_data"
+# Create file in home directory “testuser_data” owned by “testuser”
+touch /home/testuser_data
+chown testuser:testuser /home/testuser_data
