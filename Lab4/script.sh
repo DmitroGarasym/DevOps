b98fd2782a956e7720b41890156c0c9a5dc93148
Дмитро Гарасим <94192861+DmitroGarasym@users.noreply.github.com>
2021-12-03 11:30:27 +0200

    Update script.sh

--- Lab4/script.sh
+++ Lab4/script.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# create user “adminuser”
+# Create user “adminuser”
 useradd adminuser
 
-# set password for “adminuser”
- echo "linuxpassword" | passwd --stdin adminuser
+# Set password for “adminuser”
+echo 'adminuser:secret' | sudo chpasswd
 
 # Create group admin
-groupadd lnxadmin
+groupadd admin
 
-echo '%lnxadmin ALL=(ALL:ALL) ALL' >> /etc/sudoers
+echo '%admin ALL=(ALL:ALL) ALL' >> /etc/sudoers
 
 # Grant for “adminuser” sudoer permission
-usermod -a -G lnxadmin adminuser
+usermod -a -G admin adminuser
 
-# create user “poweruser”
+# Create user “poweruser”
 useradd poweruser
 
-# grant for “poweruser” permission for iptables command
+# Grant for “poweruser” permission for iptables command
 echo 'poweruser ALL=(ALL:ALL) /usr/sbin/iptables' >> /etc/sudoers
 
 # Allow “poweruser” to read home directory of “adminuser” 
 usermod -a -G adminuser poweruser
 
 # List all of files with SUID bit set
-find / -type d -perm 4000 -exec ls -ld {} \;
+find / -type d -perm 4000 -exec ls -ld {} \; 1>> /var/suid_files.txt 2> /dev/null
